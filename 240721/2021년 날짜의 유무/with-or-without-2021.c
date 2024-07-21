d5ddf577bc350f9e1c765f8f4320155e6813d9d1
uh2yun9 <uhyung182@gmail.com>
2024-07-21 15:28:34 +0900

    Title: 2021년 날짜의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/2021년 날짜의 유무/with-or-without-2021.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int exist_date(int m, int d)
+{
+    if (m == 4 && m == 6 && m == 9 && m == 11) {
+        if (d < 1 || d > 30) {
+            return 0;
+        }
+    } else if (m == 2) {
+        if (d > 28) {
+            return 0;
+        }
+    } else {
+        if (d < 1 || d > 31) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m = 0, d = 0;
+    scanf("%d %d", &m, &d);
+    if (exist_date(m, d)) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+    return 0;
+}
