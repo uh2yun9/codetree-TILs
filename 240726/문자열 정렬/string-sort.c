5e9eb9c4e6b1caa7baea74e8759bc081048f3024
uh2yun9 <uhyung182@gmail.com>
2024-07-26 22:03:27 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/문자열 정렬/string-sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+void sortString(char str[])
+{
+    int n = strlen(str);
+    char temp;
+
+    // 버블 정렬
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str[100];
+    scanf("%s", str);
+    sortString(str);
+    printf("%s", str);
+    return 0;
+}
