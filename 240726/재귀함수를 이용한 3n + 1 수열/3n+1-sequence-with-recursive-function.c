efb7ebcc5122e3afc82630810d0b2e2286026abb
uh2yun9 <uhyung182@gmail.com>
2024-07-26 17:56:22 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int F(int n, int tot)
+{
+    if (n == 1) {
+        return tot;
+    }
+
+    if (n % 2 == 0) {
+        tot += 1;
+        return F(n / 2, tot);
+    } else {
+        tot += 1;
+        return F((n * 3) + 1, tot);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0, tot = 0;
+    scanf("%d", &n);
+    printf("%d", F(n, tot));
+    return 0;
+}
