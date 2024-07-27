59186599a4fa81f32d4af3ebe5c060eaaf8b517c
uh2yun9 <uhyung182@gmail.com>
2024-07-27 16:35:53 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240727/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
+++ 240727/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+int compareChars(const void *a, const void *b)
+{
+    return (*(char*)a - *(char*)b);
+}
 
 void sortString(char str[])
 {
     int n = strlen(str);
-    char temp;
-
-    // 버블 정렬
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (str[j] > str[j + 1]) {
-                temp = str[j];
-                str[j] = str[j + 1];
-                str[j + 1] = temp;
-            }
-        }
-    }
+    qsort(str, n, sizeof(char), compareChars);
 }
 
 void compareString(char a[], char b[])
@@ -32,8 +27,8 @@ void compareString(char a[], char b[])
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    char sa[10000];
-    char sb[10000];
+    char sa[100];
+    char sb[100];
     scanf("%s", sa);
     scanf("%s", sb);
     
