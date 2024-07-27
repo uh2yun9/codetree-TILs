7d65dc9dd6c398760b40b2e2cbb4156a5a9a552b
uh2yun9 <uhyung182@gmail.com>
2024-07-27 17:17:03 +0900

    Title: k번째로 신기한 문자열, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240727/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,17 @@
+n, k, t = tuple(input().split())
+n, k = int(n), int(k)
+
+def starts_with(a, b):
+    if len(a) < len(b):
+        return False
+
+    return a[:len(b)] == b
+
+words = []
+for _ in range(n):
+    word = input()
+    if (starts_with(word, t)):
+        words.append(word)
+
+words.sort()
+print(words[k - 1])
